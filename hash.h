cf8bb9cff866e5e490eebd8e8563843717851cf6
Boostibot <70029477+Boostibot@users.noreply.github.com>
2024-06-28 10:58:33 +0200

    hash update of murmur

--- hash.h
+++ hash.h
@@ -10,27 +10,41 @@
 //Hashes a 64 bit value to 64 bit hash.
 //Note that this function is bijective meaning it can be reversed.
 //In particular 0 maps to 0.
-JOT_HASH_API uint64_t hash64(uint64_t value) 
+//source: https://stackoverflow.com/a/12996028
+JOT_HASH_API uint64_t hash64(uint64_t x) 
 {
-    //source: https://stackoverflow.com/a/12996028
-    uint64_t hash = value;
-    hash = (hash ^ (hash >> 30)) * (uint64_t) 0xbf58476d1ce4e5b9;
-    hash = (hash ^ (hash >> 27)) * (uint64_t) 0x94d049bb133111eb;
-    hash = hash ^ (hash >> 31);
-    return hash;
+    x = (x ^ (x >> 30)) * (uint64_t) 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * (uint64_t) 0x94d049bb133111eb;
+    x = x ^ (x >> 31);
+    return x;
+}
+
+JOT_HASH_API uint64_t unhash64(uint64_t x) 
+{
+    x = (x ^ (x >> 31) ^ (x >> 62)) * UINT64_C(0x319642b2d24d8ec3);
+    x = (x ^ (x >> 27) ^ (x >> 54)) * UINT64_C(0x96de1b173f119089);
+    x = x ^ (x >> 30) ^ (x >> 60);
+    return x;
 }
 
 //Hashes a 32 bit value to 32 bit hash.
 //Note that this function is bijective meaning it can be reversed.
 //In particular 0 maps to 0.
-JOT_HASH_API uint32_t hash32(uint32_t value) 
+//source: https://stackoverflow.com/a/12996028
+JOT_HASH_API uint32_t hash32(uint32_t x) 
 {
-    //source: https://stackoverflow.com/a/12996028
-    uint32_t hash = value;
-    hash = ((hash >> 16) ^ hash) * 0x119de1f3;
-    hash = ((hash >> 16) ^ hash) * 0x119de1f3;
-    hash = (hash >> 16) ^ hash;
-    return hash;
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = (x >> 16) ^ x;
+    return x;
+}
+
+JOT_HASH_API uint32_t unhash32(uint32_t x) 
+{
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = ((x >> 16) ^ x) * 0x119de1f3;
+    x = (x >> 16) ^ x;
+    return x;
 }
 
 //Mixes two prevously hashed values into one. 
@@ -57,111 +71,97 @@ JOT_HASH_API uint32_t hash64_to32(uint64_t value)
     return hash_fold64(hash64(value));
 }
 
+#include <stdint.h>
+#include <assert.h>
+#include <string.h>
+
 JOT_HASH_API uint32_t hash32_murmur(const void* key, int64_t size, uint32_t seed)
 {
-    //source https://github.com/abrandoned/murmur2/blob/master/MurmurHash2.c
-    // 'm' and 'r' are mixing constants generated offline.
-    // They're not really 'magic', they just happen to work well. 
-    const uint32_t m = 0x5bd1e995;
+    //source (modified to not use unaliagned pointers since those are UB) https://github.com/abrandoned/murmur2/blob/master/MurmurHash2.c
+    uint32_t endian_check = 0x33221100;
+    assert(*(uint8_t*) (void*) &endian_check == 0 && "Big endian machine detected! Please change this algorithm to suite your machine!");
+    assert((key == NULL) == (size == 0) && size >= 0);
+
+    const uint32_t magic = 0x5bd1e995;
     const int r = 24;
 
-    // Initialize the hash to a 'random' value
-    uint32_t h = seed ^ (uint32_t) size;
+    uint32_t hash = seed;
 
-    // Mix 4 bytes at a time into the hash
     const uint8_t* data = (const uint8_t*)key;
-
-    while(size >= 4)
+    const uint8_t* end = data + size;
+    for(; data < end - 3; data += 4)
     {
-        uint32_t k = *(uint32_t*)data;
+        uint64_t read = 0; 
+        memcpy(&read, data, sizeof read);
 
-        k *= m;
-        k ^= k >> r;
-        k *= m;
+        read *= magic;
+        read ^= read >> r;
+        read *= magic;
 
-        h *= m;
-        h ^= k;
-
-        data += 4;
-        size -= 4;
+        hash *= magic;
+        hash ^= read;
     }
 
-    // Handle the last few bytes of the input array 
-    switch(size)
+    switch(size & 3)
     {
-        case 3: h ^= data[2] << 16;
-        case 2: h ^= data[1] << 8; 
-        case 1: h ^= data[0];       
-        h *= m;
+        case 3: hash ^= data[2] << 16;
+        case 2: hash ^= data[1] << 8; 
+        case 1: hash ^= data[0];       
+        hash *= magic;
     };
 
-    // Do a few final mixes of the hash to ensure the last few
-    // bytes are well-incorporated. 
-
-    h ^= h >> 13;
-    h *= m;
-    h ^= h >> 15;
-
-    return h;
+    hash ^= hash >> 13;
+    hash *= magic;
+    hash ^= hash >> 15;
+    return hash;
 } 
 
 JOT_HASH_API uint64_t hash64_murmur(const void* key, int64_t size, uint64_t seed)
 {
-    //source https://github.com/abrandoned/murmur2/blob/master/MurmurHash2.c
-    //& big endian support: https://github.com/niklas-ourmachinery/bitsquid-foundation/blob/master/murmur_hash.cpp
-    const uint64_t m = 0xc6a4a7935bd1e995;
+    //source (modified to not use unaliagned pointers since those are UB) https://github.com/abrandoned/murmur2/blob/master/MurmurHash2.c
+    uint32_t endian_check = 0x33221100;
+    assert(*(uint8_t*) (void*) &endian_check == 0 && "Big endian machine detected! Please change this algorithm to suite your machine!");
+    assert((key == NULL) == (size == 0) && size >= 0);
+
+    const uint64_t magic = 0xc6a4a7935bd1e995;
     const int r = 47;
 
-    uint64_t h = seed ^ ((uint64_t) size * m);
+    uint64_t hash = seed ^ ((uint64_t) size * magic);
 
-    const uint64_t * data = (const uint64_t *)key;
-    const uint64_t * end = data + ((uint64_t) size/8);
-
-    bool is_big_endian = false;
-    #ifdef PLATFORM_HAS_ENDIAN_BIG
-    is_big_endian = true;
-    #endif
-
-    while(data != end)
+    const uint8_t* data = (const uint8_t *)key;
+    const uint8_t* end = data + size;
+    for(; data < end - 7; data += 8)
     {
-		uint64_t k = *data++;
-        if(is_big_endian)
-        {
-			char *p = (char *)&k;
-			char c;
-			c = p[0]; p[0] = p[7]; p[7] = c;
-			c = p[1]; p[1] = p[6]; p[6] = c;
-			c = p[2]; p[2] = p[5]; p[5] = c;
-			c = p[3]; p[3] = p[4]; p[4] = c;
-        }
+        uint64_t read = 0; 
+        memcpy(&read, data, sizeof read);
 
-        k *= m; 
-        k ^= k >> r; 
-        k *= m; 
+        read *= magic; 
+        read ^= read >> r; 
+        read *= magic; 
     
-        h ^= k;
-        h *= m; 
+        hash ^= read;
+        hash *= magic; 
     }
     
-    const uint8_t* data2 = (const uint8_t*)key;
     switch(size & 7)
     {
-        case 7: h ^= ((uint64_t) data2[6]) << 48;
-        case 6: h ^= ((uint64_t) data2[5]) << 40;
-        case 5: h ^= ((uint64_t) data2[4]) << 32;
-        case 4: h ^= ((uint64_t) data2[3]) << 24;
-        case 3: h ^= ((uint64_t) data2[2]) << 16;
-        case 2: h ^= ((uint64_t) data2[1]) << 8; 
-        case 1: h ^= ((uint64_t) data2[0]);       
-        h *= m;
+        case 7: hash ^= ((uint64_t) data[6]) << 48;
+        case 6: hash ^= ((uint64_t) data[5]) << 40;
+        case 5: hash ^= ((uint64_t) data[4]) << 32;
+        case 4: hash ^= ((uint64_t) data[3]) << 24;
+        case 3: hash ^= ((uint64_t) data[2]) << 16;
+        case 2: hash ^= ((uint64_t) data[1]) << 8; 
+        case 1: hash ^= ((uint64_t) data[0]);       
+        hash *= magic;
     };
  
-    h ^= h >> r;
-    h *= m;
-    h ^= h >> r;
-
-    return h;
+    hash ^= hash >> r;
+    hash *= magic;
+    hash ^= hash >> r;
+    return hash;
 } 
+
+
 
 #define XXHASH64_PRIME_1  0x9E3779B185EBCA87ULL
 #define XXHASH64_PRIME_2  0xC2B2AE3D27D4EB4FULL
@@ -179,18 +179,18 @@ static inline uint64_t _xxhash64_process_single(uint64_t previous, uint64_t inpu
     return _xxhash64_rotate_left(previous + input * XXHASH64_PRIME_2, 31) * XXHASH64_PRIME_1;
 }
 
-static inline uint64_t xxhash64(const void* input, uint64_t length, uint64_t seed)
+JOT_HASH_API uint64_t xxhash64(const void* key, int64_t size, uint64_t seed)
 {
     uint32_t endian_check = 0x33221100;
-    ASSERT(*(uint8_t*) (void*) &endian_check == 0 && "Big endian machine detected! Please change this algorithm to suite your machine!");
-    ASSERT((input == NULL) == (length == 0) && length >= 0);
+    assert(*(uint8_t*) (void*) &endian_check == 0 && "Big endian machine detected! Please change this algorithm to suite your machine!");
+    assert((key == NULL) == (size == 0) && size >= 0);
 
-    uint8_t* data = (uint8_t*) (void*) input;
-    uint8_t* end = data + length;
+    uint8_t* data = (uint8_t*) (void*) key;
+    uint8_t* end = data + size;
     
     //Bulk computation
     uint64_t hash = seed + XXHASH64_PRIME_5;
-    if (length >= 32)
+    if (size >= 32)
     {
         uint64_t state[4] = {0};
         uint64_t block[4] = {0};
@@ -217,7 +217,7 @@ static inline uint64_t xxhash64(const void* input, uint64_t length, uint64_t see
         hash = (hash ^ _xxhash64_process_single(0, state[2])) * XXHASH64_PRIME_1 + XXHASH64_PRIME_4;
         hash = (hash ^ _xxhash64_process_single(0, state[3])) * XXHASH64_PRIME_1 + XXHASH64_PRIME_4;
     }
-    hash += length;
+    hash += size;
 
     //Consume last <32 Bytes
     for (; data + 8 <= end; data += 8)
@@ -248,9 +248,10 @@ static inline uint64_t xxhash64(const void* input, uint64_t length, uint64_t see
 JOT_HASH_API uint32_t hash32_fnv_one_at_a_time(const void* key, int64_t size, uint32_t seed)
 {
     // Source: https://github.com/aappleby/smhasher/blob/master/src/Hashes.cpp
-    uint32_t hash = seed;
-    hash ^= 2166136261UL;
-    const uint8_t* data = (const uint8_t*)key;
+    assert((key == NULL) == (size == 0) && size >= 0);
+
+    uint32_t hash = seed ^ 2166136261UL;
+    const uint8_t* data = (const uint8_t*) key;
     for(int64_t i = 0; i < size; i++)
     {
         hash ^= data[i];
@@ -258,7 +259,5 @@ JOT_HASH_API uint32_t hash32_fnv_one_at_a_time(const void* key, int64_t size, ui
     }
     return hash;
 }
-
-
 
 #endif
