14e94c49e8b4fd44d0e3d9d4843d283b436d671e
Boostibot <70029477+Boostibot@users.noreply.github.com>
2025-02-07 02:48:57 +0100

    perf and time

--- perf.h
+++ perf.h
@@ -1,54 +1,152 @@
 #ifndef MODULE_PERF
 #define MODULE_PERF
 
-//TODO MAKE SIMPLER
+#ifndef EXTERNAL
+	#define EXTERNAL 
+#endif
 
 #include <stdbool.h>
 #include <math.h>
 #include <stdint.h>
 
-#ifndef ASSERT
-	#include <assert.h>
-	#define ASSERT(x) assert(x)
-#endif
-
-#ifndef EXTERNAL
-	#define EXTERNAL 
-#endif
-
-typedef struct Benchmark_Stats {
-	int64_t runs;
-	int64_t batch_size;
-
-	double total_s;
-	double average_s;
-	double min_s;
-	double max_s;
-} Benchmark_Stats;
-
-typedef struct Benchmark {
-	int64_t start;
-	int64_t duration_end;
-	int64_t warmup_end;
-	int64_t frequency;
-
-	//Chnages on every iteration!
-	int64_t iter;
-	int64_t iter_begin_time;
-	int64_t time_sum;
-	int64_t time_min;
-	int64_t time_max;
-} Benchmark;
-
-//Maintains a benchmark. See example below for how to use this.
-EXTERNAL bool benchmark_with_custom_warmup(Benchmark* bench, double warmup, double time);
-EXTERNAL bool benchmark(Benchmark* bench, double time);
-EXTERNAL Benchmark_Stats benchmark_get_stats(Benchmark bench, int64_t batch_size);
+static inline int64_t perf_counter();
+static inline int64_t perf_counter_freq();
+static inline int64_t perf_rdtsc();
+static inline void    perf_rdtsc_barrier();
 
 //Prevents the compiler from otpimizing away the variable (and thus its value) pointed to by ptr.
-static void benchmark_do_not_optimize(const void* ptr);
+static inline void perf_do_not_optimize(const void* ptr);
 
-static void perf_do_not_optimize(const void* ptr) 
+//A very simple benchmark optimized for absolutele developer convenience. See bench_example
+typedef struct Quickbench {
+	int64_t runs;
+    int64_t rdtsc_freq;
+	double total;
+	double average;
+	double min;
+	double max;
+    double actual_duration;
+    double duration;
+    double warmup;
+    int64_t _internal[16];
+} Quickbench;
+
+static inline bool quickbench(Quickbench* stats, double duration);
+static inline bool quickbench_with_explicit_warmup(Quickbench* stats, double duration, double warmup);
+EXTERNAL int64_t calculate_tsc_freq(int64_t qpc_dur, int64_t tsc_dur);
+
+#if 0
+static void bench_example()
+{
+    Quickbench bench = {0};
+    while(quickbench(&bench, 1.0)) {
+        //this code is measured
+        int64_t val = 1000 % bench.runs;
+        perf_do_not_optimize(&val);
+    }
+    printf("average:%lfns min:%lfns\n", bench.average*1e9, bench.min*1e9);
+}
+#endif
+
+//Nasty nasty inline implementation below =========================
+#if defined(_WIN32) || defined(_WIN64)
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+    typedef union _LARGE_INTEGER LARGE_INTEGER;
+    __declspec(dllimport) int __stdcall QueryPerformanceCounter(LARGE_INTEGER* out);
+    __declspec(dllimport) int __stdcall QueryPerformanceFrequency(LARGE_INTEGER* out);
+    #ifdef __cplusplus
+    }
+    #endif
+    
+    static inline int64_t perf_counter()
+    {
+        int64_t out = 0;
+        QueryPerformanceCounter((LARGE_INTEGER*) (void*) &out);
+        return out;
+    }
+
+    static inline int64_t perf_counter_freq()
+    {
+        static int64_t freq = 0;
+        if(freq == 0) QueryPerformanceFrequency((LARGE_INTEGER*) (void*) &freq);
+        return freq;
+    }
+    
+#elif defined(__linux__) || (defined(__APPLE__) && defined(__MACH__))
+    #include <time.h>
+    static inline int64_t perf_counter()
+    {
+        struct timespec ts = {0};
+        (void) clock_gettime(CLOCK_MONOTONIC_RAW , &ts);
+        return (int64_t) ts.tv_nsec + ts.tv_sec * 1000000000LL;
+    }
+
+    static inline int64_t perf_counter_freq()
+    {
+        return (int64_t) 1000000000LL;
+    }
+#else
+    #error unsupported platform!
+#endif
+
+#if defined(__x86_64__) || defined(_M_X64) || defined(__amd64__) && !defined(_M_ARM64EC) || defined(_M_CEE_PURE) || defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+    #ifdef _MSC_VER
+        #include <intrin.h>
+        static inline int64_t perf_rdtsc(){ 
+            _ReadWriteBarrier(); 
+            return (int64_t) __rdtsc(); 
+        }
+        static inline void perf_rdtsc_barrier(){ 
+            _ReadWriteBarrier(); 
+            _mm_lfence();
+        }
+    #else
+        #include <x86intrin.h>
+        static inline int64_t perf_rdtsc(){ 
+            __asm__ __volatile__("":::"memory");
+            return (int64_t) __rdtsc(); 
+        }
+        static inline void perf_rdtsc_barrier(){ 
+            __asm__ __volatile__("":::"memory");
+            _mm_lfence();
+        }
+    #endif
+#elif defined(_M_ARM64) || defined(_M_ARM64EC) || defined(__aarch64__) || defined(__ARM_ARCH_ISA_A64)
+    #if defined(_MSC_VER) && !defined(__clang__)
+        #include <intrin.h>
+    #endif
+
+    //msvc version taken from: https://gist.github.com/mmozeiko/98bb947fb5a9d5b8a695adf503308a58#file-armv8_tsc-h-L19-L45
+    //inline assembly Adapted from: https://github.com/cloudius-systems/osv/blob/master/arch/aarch64/arm-clock.cc
+    static inline int64_t perf_rdtsc() {
+        //Please note we read CNTVCT cpu system register which provides
+        //the accross-system consistent value of the virtual system counter.
+        int64_t cntvct;
+        #if defined(_MSC_VER) && !defined(__clang__)
+            // "Accessing CNTVCT_EL0" in https://developer.arm.com/documentation/ddi0601/latest/AArch64-Registers/CNTVCT-EL0--Counter-timer-Virtual-Count-Register
+            cntvct = _ReadStatusReg(ARM64_SYSREG(3, 3, 14, 0, 2));
+        #else
+            asm volatile ("mrs %0, cntvct_el0; " : "=r"(cntvct) :: "memory");
+        #endif
+        return cntvct;
+    }
+
+    static inline void perf_rdtsc_barrier() {
+        #if defined(_MSC_VER) && !defined(__clang__)
+            __isb(_ARM64_BARRIER_SY);
+        #else
+            asm volatile ("isb;" ::: "memory");
+        #endif
+    }
+#else
+    #define PERF_TSC_FALLBACK	
+    static inline int64_t perf_rdtsc()          {return perf_counter();}
+    static inline void    perf_rdtsc_barrier()  {}
+#endif
+
+static inline void perf_do_not_optimize(const void* ptr) 
 { 
 	#if defined(__GNUC__) || defined(__clang__)
 		__asm__ __volatile__("" : "+r"(ptr));
@@ -69,18 +167,137 @@ static void perf_do_not_optimize(const void* ptr)
 	#endif
 }
 
-	
-
-
+#if defined(_MSC_VER)
+    #define ATTRIBUTE_INLINE_NEVER  __declspec(noinline)
+    #define ATTRIBUTE_NO_CHECK      __declspec(safebuffers) 
+#elif defined(__GNUC__) || defined(__clang__)
+    #define ATTRIBUTE_INLINE_NEVER  __attribute__((noinline))
+    #define ATTRIBUTE_NO_CHECK 
+#else
+    #define ATTRIBUTE_INLINE_NEVER                              
+    #define ATTRIBUTE_NO_CHECK 
 #endif
 
-#define MODULE_IMPL_ALL
+ATTRIBUTE_INLINE_NEVER ATTRIBUTE_NO_CHECK
+EXTERNAL bool _quickbench_explicit(Quickbench* stats, double duration, double warmup);
+
+static inline bool quickbench(Quickbench* stats, double duration) 
+{ 
+    return _quickbench_explicit(stats, duration, -1); 
+}
+
+static inline bool quickbench_with_explicit_warmup(Quickbench* stats, double duration, double warmup)
+{
+    return _quickbench_explicit(stats, duration, warmup); 
+}
+#endif
 
 #if (defined(MODULE_IMPL_ALL) || defined(MODULE_IMPL_PERF)) && !defined(MODULE_HAS_IMPL_PERF)
 #define MODULE_HAS_IMPL_PERF
 
+EXTERNAL int64_t calculate_tsc_freq(int64_t qpc_dur, int64_t tsc_dur)
+{
+    //duration = qpc_dur/qpc_freq = tsc_dur/tsc_freq
+    // => tsc_freq = qpc_freq*tsc_dur/qpc_dur
+    #if defined(_MSC_VER) && !defined(__clang__)
+        int64_t qpc_freq = perf_counter_freq();
+        uint64_t hi, lo = _umul128((uint64_t) qpc_freq, (uint64_t) tsc_dur, &hi);
+        uint64_t rem, quo = _udiv128(hi, lo, (uint64_t) qpc_dur, &rem);
+        return (int64_t) quo;
+    #else
+        return (int64_t) ((__uint128_t)qpc_freq * (__uint128_t)tsc_dur/ (__uint128_t)qpc_dur);
+    #endif
+}
 
+ATTRIBUTE_INLINE_NEVER ATTRIBUTE_NO_CHECK
+EXTERNAL bool _quickbench_explicit(Quickbench* stats, double duration, double warmup)
+{
+    int64_t after = perf_rdtsc();
+    perf_rdtsc_barrier();
+    
+    typedef struct _Quickbench_Internal {
+        bool is_init;
+	    bool is_after_warmup;
+        bool _[6];
 
+	    int64_t iter_begin_tsc;
+	    int64_t time_sum;
+	    int64_t time_min;
+	    int64_t time_max;
 
-	#endif
+	    int64_t warmup_end_qpc;
+	    int64_t duration_end_tsc;
+        int64_t warmup_tsc_freq_estimate;
+    
+	    int64_t begin_qpc;
+	    int64_t begin_tsc;
+	    int64_t end_qpc;
+	    int64_t end_tsc;
+    } _Quickbench_Internal;
+
+    _Quickbench_Internal* bench = (_Quickbench_Internal*) (void*) stats->_internal;
+    int64_t before = bench->iter_begin_tsc;
+    if(bench->is_after_warmup)
+    {
+        int64_t diff = after - before;
+        bench->time_sum += diff; 
+        bench->time_min = bench->time_min < diff ? bench->time_min : diff;
+        bench->time_max = bench->time_max > diff ? bench->time_max : diff;
+        stats->runs += 1;
+    }
+    else
+    {
+        int64_t now_qpc = perf_counter();
+        int64_t now_tsc = perf_rdtsc();
+        perf_rdtsc_barrier();
+        if(bench->is_init == false)
+        {
+            bench->is_init = true;
+            bench->begin_tsc = now_tsc;
+            bench->begin_qpc = now_qpc;
+            bench->time_min = INT64_MAX;
+            bench->time_max = INT64_MIN;
+            bench->duration_end_tsc = INT64_MAX;
+            stats->duration = duration;
+            stats->warmup = warmup < 0 ? duration/10 : warmup;
+            bench->warmup_end_qpc = now_qpc + (int64_t) (stats->warmup*perf_counter_freq());
+        }
+        if(now_qpc > bench->warmup_end_qpc)
+        {
+            int64_t qpc_warmup_dur = now_qpc - bench->begin_qpc;
+            int64_t tsc_warmup_dur = now_tsc - bench->begin_tsc;
+            int64_t freq = calculate_tsc_freq(qpc_warmup_dur, tsc_warmup_dur);
+            bench->is_after_warmup = true;
+            bench->warmup_tsc_freq_estimate = freq;
+            bench->duration_end_tsc = bench->begin_tsc + (int64_t) (duration*freq);
+        }
+    }
+    
+    if(after > bench->duration_end_tsc)
+    {
+        bench->end_qpc = perf_counter();
+        bench->end_tsc = perf_rdtsc();
+        perf_rdtsc_barrier();
+
+        int64_t freq = calculate_tsc_freq(bench->end_qpc - bench->begin_qpc, bench->end_tsc - bench->begin_tsc);
+        stats->actual_duration = (double)(bench->end_qpc - bench->begin_qpc)/perf_counter_freq();
+        stats->rdtsc_freq = freq;
+	    stats->total = 0;
+	    stats->average = 0;
+	    stats->min = 0;
+	    stats->max = 0;
+        if(stats->runs > 0 && freq > 0) {
+            stats->total = (double) bench->time_sum/freq;
+            stats->average = (double) (bench->time_sum/stats->runs)/freq;
+            stats->min = (double) bench->time_min/freq;
+            stats->max = (double) bench->time_max/freq;
+        } 
+        return false;
+    }
+
+    perf_rdtsc_barrier();
+    bench->iter_begin_tsc = perf_rdtsc();
+    return true;
+}
+
 #endif
--- time.h
+++ time.h
@@ -23,13 +23,13 @@
 #define WEEK_SECONDS            604800ll
 #define YEAR_SECONDS            31556952ll
 
-TIMEAPI int64_t perf_counter();     //returns precise as possible yet long term stable time since unspecified point in time (last boot) 
-TIMEAPI int64_t perf_frequency();   //returns the frequency of perf_counter
-TIMEAPI int64_t clock_ns();         //returns time in nanoseconds since since unspecified point in time (last boot) 
 TIMEAPI int64_t epoch_time();       //returns time since the epoch in microseconds
-TIMEAPI double  clock_seconds();    //returns time in seconds since last call to clock_seconds_set(x) plus x. 
-TIMEAPI float   clock_secondsf();   //returns time in seconds since last call to clock_seconds_set(x) plus x as float
-TIMEAPI double  clock_seconds_set(double to_time); //sets the base for clock_seconds and return the previous time 
+TIMEAPI int64_t clock_ticks();      //returns precise as possible yet long term stable time since unspecified point in time (last boot) 
+TIMEAPI int64_t clock_ticks_freq(); //returns the frequency of clock_ticks
+TIMEAPI int64_t clock_ns();         //returns time in nanoseconds since since unspecified point in time (last boot) 
+EXTERNAL double clock_seconds();    //returns time in seconds since last call to clock_seconds_set(x) plus x. 
+EXTERNAL float  clock_secondsf();   //returns time in seconds since last call to clock_seconds_set(x) plus x as float
+EXTERNAL double clock_seconds_set(double to_time); //sets the base for clock_seconds and return the previous time 
 
 //@NOTE: 
 //For clock_seconds we might be scared that the int64_t to double conversion will cost us precision for sufficiently large 
@@ -44,7 +44,6 @@ TIMEAPI double  clock_seconds_set(double to_time); //sets the base for clock_sec
 //INLINE IMPLEMENTATION
 #if (defined(MODULE_IMPL_ALL) || defined(TIMEAPI_INLINE_IMPL)) && !defined(MODULE_HAS_INLINE_IMPL_FILE)
 #define MODULE_HAS_INLINE_IMPL_FILE
-
     extern int     g_clock_seconds_init;
     extern int64_t g_clock_seconds_offset;
     extern double  g_clock_seconds_period;
@@ -55,24 +54,23 @@ TIMEAPI double  clock_seconds_set(double to_time); //sets the base for clock_sec
         #ifdef __cplusplus
         extern "C" {
         #endif
-        typedef int BOOL;
         typedef union _LARGE_INTEGER LARGE_INTEGER;
         typedef struct _FILETIME FILETIME;
-        __declspec(dllimport) BOOL __stdcall QueryPerformanceCounter(LARGE_INTEGER* out);
-        __declspec(dllimport) BOOL __stdcall QueryPerformanceFrequency(LARGE_INTEGER* out);
+        __declspec(dllimport) int __stdcall QueryPerformanceCounter(LARGE_INTEGER* out);
+        __declspec(dllimport) int __stdcall QueryPerformanceFrequency(LARGE_INTEGER* out);
         __declspec(dllimport) void __stdcall GetSystemTimeAsFileTime(FILETIME* lpSystemTimeAsFileTime);
         #ifdef __cplusplus
         }
         #endif
 
-        TIMEAPI int64_t perf_counter()
+        TIMEAPI int64_t clock_ticks()
         {
             int64_t out = 0;
             QueryPerformanceCounter((LARGE_INTEGER*) (void*) &out);
             return out;
         }
 
-        TIMEAPI int64_t perf_frequency()
+        TIMEAPI int64_t clock_ticks_freq()
         {
             if(g_clock_seconds_freq == 0)
                 QueryPerformanceFrequency((LARGE_INTEGER*) (void*) &g_clock_seconds_freq);
@@ -83,16 +81,15 @@ TIMEAPI double  clock_seconds_set(double to_time); //sets the base for clock_sec
         {
             uint64_t filetime = 0;
             GetSystemTimeAsFileTime((FILETIME*) (void*) &filetime);
-            uint64_t epoch_time = filetime / 10 - 11644473600000000LL;
-            return epoch_time;
+            return filetime / 10 - 11644473600000000LL;
         }
         
         #if defined(_MSC_VER) && !defined(__clang__)
             #include <intrin.h>
             __declspec(noinline) static int64_t _clock_ns_unusual()
             {
-                uint64_t counter = (uint64_t) perf_counter();
-                uint64_t freq = (uint64_t) perf_frequency(); 
+                uint64_t counter = (uint64_t) clock_ticks();
+                uint64_t freq = (uint64_t) clock_ticks_freq(); 
                 uint64_t hi, lo = _umul128(counter, 1000000000ull, &hi);
                 uint64_t rem, quo = _udiv128(hi, lo, freq, &rem);
                 return (int64_t) quo;
@@ -100,8 +97,8 @@ TIMEAPI double  clock_seconds_set(double to_time); //sets the base for clock_sec
         #else
             __atribute__((noinline)) static int64_t _clock_ns_unusual()
             {
-                uint64_t counter = (uint64_t) perf_counter();
-                uint64_t freq = (uint64_t) perf_frequency(); 
+                uint64_t counter = (uint64_t) clock_ticks();
+                uint64_t freq = (uint64_t) clock_ticks_freq(); 
                 return (int64_t) ((__uint128_t) counter*1000000000ull/freq);
             }
         #endif
@@ -112,66 +109,57 @@ TIMEAPI double  clock_seconds_set(double to_time); //sets the base for clock_sec
             if(g_clock_seconds_freq != 10000000) 
                 return _clock_ns_unusual();
 
-            return perf_counter()*100;
+            return clock_ticks()*100;
         }
 
     #elif defined(__linux__) || (defined(__APPLE__) && defined(__MACH__))
         #include <time.h>
-        TIMEAPI int64_t perf_counter()
+        TIMEAPI int64_t clock_ticks()
         {
             struct timespec ts = {0};
             (void) clock_gettime(CLOCK_MONOTONIC_RAW , &ts);
             return (int64_t) ts.tv_nsec + ts.tv_sec * 1000000000LL;
         }
-
-        TIMEAPI int64_t perf_frequency()
-        {
-            return (int64_t) 1000000000LL;
-        }
-
-        TIMEAPI int64_t epoch_time()
-        {
-            return perf_counter()/1000;
-        }
-
-        TIMEAPI int64_t clock_ns()
-        {
-            return perf_counter();
+        TIMEAPI int64_t clock_ticks_freq() { return (int64_t) 1000000000LL; }
+        TIMEAPI int64_t clock_ns()         { return clock_ticks(); }
+        TIMEAPI int64_t epoch_time()       
+        { 
+            struct timespec ts = {0};
+            (void) clock_gettime(CLOCK_REALTIME , &ts);
+            return (int64_t) ts.tv_nsec/1000 + ts.tv_sec*1000000LL;
         }
     #endif
-
-    static double clock_seconds()
-    {
-        if(g_clock_seconds_init == 0)
-            clock_seconds_set(0);
-        double now = (double) (perf_counter() - g_clock_seconds_offset)*g_clock_seconds_period;
-        return now;
-    }
-
-    static float clock_secondsf()
-    {
-        return (float) clock_seconds();
-    }
+    
 #endif
 
 #if (defined(MODULE_IMPL_ALL) || defined(MODULE_IMPL_TIME)) && !defined(MODULE_HAS_IMPL_TIME)
 #define MODULE_HAS_IMPL_TIME
-        
     int     g_clock_seconds_init = 0;
     int64_t g_clock_seconds_offset = 0;
     double  g_clock_seconds_period = 0;
     int64_t g_clock_seconds_freq = 0;
     EXTERNAL double clock_seconds_set(double to_time)
     {
-        int64_t counter = perf_counter();
+        int64_t counter = clock_ticks();
         double prev_now = (double) (counter - g_clock_seconds_offset)*g_clock_seconds_period;
-        int64_t freq = perf_frequency();
+        int64_t freq = clock_ticks_freq();
         g_clock_seconds_offset = counter + (int64_t) (to_time*freq + 0.5);
         g_clock_seconds_period = 1.0 / freq;
         g_clock_seconds_freq = freq;
         g_clock_seconds_init = 1;
         return prev_now;
     }
+    
+    EXTERNAL double clock_seconds()
+    {
+        if(g_clock_seconds_init == 0)
+            clock_seconds_set(0);
+        return (double) (clock_ticks() - g_clock_seconds_offset)*g_clock_seconds_period;
+    }
 
+    EXTERNAL float clock_secondsf()
+    {
+        return (float) clock_seconds();
+    }
 #endif
 
